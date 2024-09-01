eabae18dc484413f5e02681fd003a929f30762f4
Kailas-lab <kailashraina9@gmail.com>
2024-09-01 16:00:18 +0530

    updated-render-files

--- Template variables/template/syntax/urls.py
+++ Template variables/template/syntax/urls.py
@@ -1,6 +1,10 @@
+# urls.py
 from django.urls import path
-from . import views
+from .views import all_members, member_details
+from .import views 
 
 urlpatterns = [
-    path('', views.testing, name='testing'),
+    path('',views.testing,name='testing'),
+    path('members/', all_members, name='all_members'),  # URL for the list of members
+    path('members/details/<int:member_id>/', member_details, name='member_details'),  # URL for member details
 ]
--- Template variables/template/syntax/views.py
+++ Template variables/template/syntax/views.py
@@ -1,16 +1,52 @@
-from enum import member
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Member
 
-# Create your views here.
+# views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Member  # Replace with your actual model import
+
+def all_members(request):
+    members = Member.objects.all()  # Retrieve all members from the database
+    context = {
+        'mymembers': members
+    }
+    return render(request, 'all_members.html', context)
+
+def member_details(request, member_id):
+    member = get_object_or_404(Member, id=member_id)  # Retrieve a specific member by ID
+    context = {
+        'mymember': member
+    }
+    return render(request, 'details.html', context)
+
 
 
 def testing(request):
-  mymembers= Member.objects.all().values()
   template = loader.get_template('template.html')
   context = {
-    'mymembers': mymembers,
-  }
-  return HttpResponse(template.render(context, request))
+    'cars': [
+      {
+        'brand': 'Ford',
+        'model': 'Mustang',
+        'year': '1964',
+      },
+      {
+        'brand': 'Ford',
+        'model': 'Bronco',
+        'year': '1970',
+      },
+      {
+        'brand': 'Volvo',
+        'model': 'P1800',
+        'year': '1964',
+      }]
+    }
+  return HttpResponse(template.render(context, request)) 
+
+
+
+
+
+
+
